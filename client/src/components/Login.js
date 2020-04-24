b73d1dd8b125b9cde9d2ac231dabc9e3125350aa
Gordon Caister <gordoncaister@gmail.com>
2020-04-24 10:42:50 -0500

    created PrivateRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { useInput } from "../utils/useInput";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const [username, setUsername, handleUsername] = useInput("");
-  const [password, setPassword, handlePassword] = useInput("");
-
+  const [username, setUsername, handleUsername] = useInput("Lambda School");
+  const [password, setPassword, handlePassword] = useInput("i<3Lambd4");
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,13 +17,16 @@ const Login = () => {
     .post("/api/login",{username:username,password:password})
     .then(res =>{
       console.log(res)
+      localStorage.setItem('token',res.data.payload)
+      props.history.push('/')
     })
+    .catch(err => console.log(err))
 
   }
 
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
+      <h1 style={{textAlign:"center"}}>Welcome to the Bubble App!</h1>
       <form onSubmit={handleSubmit}>
         <input
           label="Username"
