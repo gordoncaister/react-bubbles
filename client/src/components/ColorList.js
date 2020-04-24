267bbb20fd071761c58432394ccc7dd25731d268
Gordon Caister <gordoncaister@gmail.com>
2020-04-24 11:19:23 -0500

    edit is working as intended

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const initialColor = {
   color: "",
@@ -10,7 +12,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
-
+  const history = useHistory()
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
@@ -18,12 +20,33 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    console.log(e.target)
+    console.log(colorToEdit.id)
+    axiosWithAuth()
+    .put(`/api/colors/${colorToEdit.id}`,colorToEdit)
+    .then (res => {
+      console.log(res)
+      history.go(0)
+    })
+    .catch(err => {
+      console.log(err)
+    })
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
   const deleteColor = color => {
+    // axiosWithAuth()
+    // .delete(`/api/colors/}`,color)
+    // .then((res)=>{
+    //   history.push("/")
+    //   history.go(-1)
+    //   console.log(res)
+    // })
+    // .catch(err => {
+    //   console.log(err)
+    // })
     // make a delete request to delete this color
   };
 
