36e52855711d389017cb71107e45200ce993ee38
Gordon Caister <gordoncaister@gmail.com>
2020-04-24 10:22:26 -0500

    answered questions, began to create login form, created utils> axiosWithAuth and useInput for easier use of forms

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useInput } from "../utils/useInput";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [username, setUsername, handleUsername] = useInput("");
+  const [password, setPassword, handlePassword] = useInput("");
+
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <input
+          label="Username"
+          type="text"
+          name="username"
+          placeholder="username"
+          value={username}
+          onChange={handleUsername}
+        />
+        <br />
+        <input
+          label="Password"
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={handlePassword}
+        />
+      </form>
     </>
   );
 };
